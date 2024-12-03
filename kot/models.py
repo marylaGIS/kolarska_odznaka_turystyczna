99abed1dde217a34856ce0b1eaaf864e64ed0832
User <marylaGIS@github.com>
2024-12-03 21:52:41 +0100

    created models: place, tourist

--- kot/models.py
+++ kot/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Place(models.Model):
+    voivodeship = models.CharField(max_length=30)
+    localization = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    link1 = models.CharField(max_length=255, blank=True, null=True)
+    link2 = models.CharField(max_length=255, blank=True, null=True)
+    geom = models.PointField(srid=4326)
+
+
+class Tourist(models.Model):
+    home = models.CharField(max_length=100, blank=True, null=True)
+    geom = models.PointField(srid=4326, blank=True, null=True)
+    public = models.BooleanField(default=False)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    places = models.ManyToManyField('Place', through='TouristsPlaces')
+
+
+class TouristsPlaces(models.Model):
+    tourist = models.ForeignKey('Tourist', on_delete=models.CASCADE)
+    place = models.ForeignKey('Place', on_delete=models.CASCADE)
+    visit_date = models.DateField()
+
