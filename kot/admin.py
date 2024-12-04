d1ab62f6330a234f3c3b871a63149b1cea737623
User <marylaGIS@github.com>
2024-12-04 20:45:51 +0100

    Place and Tourist models registered in admin panel

--- kot/admin.py
+++ kot/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
 
-# Register your models here.
+from .models import Place, Tourist
+
+
+class PlaceAdmin(LeafletGeoAdmin):
+    list_display = ['id', 'voivodeship', 'localization',
+                    'name', 'geom', 'link1', 'link2']
+    
+
+class TouristAdmin(LeafletGeoAdmin):
+    list_display = ['home', 'geom', 'public', 'user']
+
+
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(Tourist, TouristAdmin)
