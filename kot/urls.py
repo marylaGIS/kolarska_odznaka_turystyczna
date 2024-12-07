bfd91a9e7b3d95741a6c849879b3797475d928e6
User <marylaGIS@github.com>
2024-12-07 20:17:54 +0100

    sing-up, sign-in, logout

--- kot/urls.py
+++ kot/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import IndexView, MapView, ListView, PlaceDetailsView
+from .views import IndexView, MapView, ListView, PlaceDetailsView, \
+    SignUpView, SignInView, LogoutView
 
 from .views import DatasetPlacesView
 
@@ -9,6 +10,10 @@ urlpatterns = [
     path(r'mapa/', MapView.as_view(), name='map'),
     path(r'lista/', ListView.as_view(), name='list'),
     path(r'obiekt/<int:place_id>', PlaceDetailsView.as_view(), name='place-details'),
+
+    path(r'zarejestruj', SignUpView.as_view(), name='register'),
+    path(r'zaloguj', SignInView.as_view(), name='login'),
+    path(r'wyloguj', LogoutView.as_view(), name='logout'),
 
     path(r'dataset-places/', DatasetPlacesView.as_view(), name='dataset-places'),
 ]
--- kot/views.py
+++ kot/views.py
@@ -1,9 +1,13 @@
 from django.core.serializers import serialize
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
 from django.views import View
 
-from .models import Place
+from .forms import SignUpForm
+from .models import Place, Tourist
 
 
 class IndexView(View):
@@ -29,6 +33,35 @@ class PlaceDetailsView(View):
 
         return render(request, 'place-details.html', ctx)
 
+
+class SignUpView(View):
+    def get(self, request):
+        form = SignUpForm()
+        return render(request, 'sign-up.html', {'form': form})
+    
+    def post(self, request):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            if not User.objects.filter(username=username).exists():
+                form.save()
+            user = User.objects.get(username=username)
+            Tourist.objects.create(user=user)
+        else:
+            return redirect('zarejestruj')
+
+        return redirect('login')
+
+
+class SignInView(LoginView):
+    template_name = 'sign-in.html'
+    next_page = '/'
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('/')
 
 
 class DatasetPlacesView(View):
